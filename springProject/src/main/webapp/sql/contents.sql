61a37643640f83e8d903e15ce62a0c03400a0e79
SUJIN <cainstear@gmail.com>
2022-03-17 17:09:35 +0900

    컨텐츠 쪽 테이블 sql문

--- springProject/src/main/webapp/sql/contents.sql
+++ springProject/src/main/webapp/sql/contents.sql
@@ -1,1 +1,28 @@
-/* 테스트용 */
+/* 컨텐츠 좋아요 테이블 */
+create table dcontents_like(
+  clike_num number not null,
+  contents_num number not null,
+  mem_num number not null,
+  constraint dcontents_like_fk foreign key (mem_num) 
+                         references dmember (mem_num)
+);
+
+/* 컨텐츠 평가(별점) 테이블*/
+create table dcontents_grade(
+  grade_num number not null,
+  contents_num number not null,
+  grade number(1) not null,
+  mem_num number not null,
+  constraint dcontents_like_fk foreign key (mem_num) 
+                         references dmember (mem_num)
+);
+
+/* 컨텐츠 캘린더 테이블 */
+create table dcontents_cal(
+  cal_num number not null,
+  custom_date date not null,
+  contents_num number not null,
+  mem_num number not null,
+  constraint dcontents_like_fk foreign key (mem_num) 
+                         references dmember (mem_num)
+);
