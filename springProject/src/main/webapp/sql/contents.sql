c4ef8abea6c38a74bb69525834fe3ad6d71f5e74
SUJIN <cainstear@gmail.com>
2022-03-17 17:19:50 +0900

    리뷰 테이블 sql문

--- springProject/src/main/webapp/sql/contents.sql
+++ springProject/src/main/webapp/sql/contents.sql
@@ -3,9 +3,11 @@ create table dcontents_like(
   clike_num number not null,
   contents_num number not null,
   mem_num number not null,
+  constraint dcontents_like_pk primary key (clike_num),
   constraint dcontents_like_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_like_seq;
 
 /* 컨텐츠 평가(별점) 테이블*/
 create table dcontents_grade(
@@ -13,9 +15,11 @@ create table dcontents_grade(
   contents_num number not null,
   grade number(1) not null,
   mem_num number not null,
-  constraint dcontents_like_fk foreign key (mem_num) 
+  constraint dcontents_grade_pk primary key (grade_num),
+  constraint dcontents_grade_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_grade_seq;
 
 /* 컨텐츠 캘린더 테이블 */
 create table dcontents_cal(
@@ -23,6 +27,8 @@ create table dcontents_cal(
   custom_date date not null,
   contents_num number not null,
   mem_num number not null,
-  constraint dcontents_like_fk foreign key (mem_num) 
+  constraint dcontents_cal_pk primary key (cal_num),
+  constraint dcontents_grade_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_cal_seq;
