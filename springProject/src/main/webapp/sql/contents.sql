fb34da2faa3ec5a831f57075e0c28eb1f3a139aa
SUJIN <cainstear@gmail.com>
2022-03-17 17:26:02 +0900

    컨텐츠 쪽 sql 수정

--- springProject/src/main/webapp/sql/contents.sql
+++ springProject/src/main/webapp/sql/contents.sql
@@ -36,9 +36,11 @@ create table dcontents_like(
   clike_num number not null,
   contents_num number not null,
   mem_num number not null,
+  constraint dcontents_like_pk primary key (clike_num),
   constraint dcontents_like_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_like_seq;
 
 /* 컨텐츠 평가(별점) 테이블*/
 create table dcontents_grade(
@@ -46,9 +48,11 @@ create table dcontents_grade(
   contents_num number not null,
   grade number(1) not null,
   mem_num number not null,
-  constraint dcontents_like_fk foreign key (mem_num) 
+  constraint dcontents_grade_pk primary key (grade_num),
+  constraint dcontents_grade_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_grade_seq;
 
 /* 컨텐츠 캘린더 테이블 */
 create table dcontents_cal(
@@ -56,6 +60,8 @@ create table dcontents_cal(
   custom_date date not null,
   contents_num number not null,
   mem_num number not null,
-  constraint dcontents_like_fk foreign key (mem_num) 
+  constraint dcontents_cal_pk primary key (cal_num),
+  constraint dcontents_grade_fk foreign key (mem_num) 
                          references dmember (mem_num)
 );
+create sequence dcontents_cal_seq;
