583647a78a56ce5f6f1d93abcef9ea150f594086
yuni0810 <tomatomayeh@gmail.com>
2022-03-17 21:09:26 +0900

    멤버vo

--- springProject/src/main/java/kr/spring/member/vo/MemberVO.java
+++ springProject/src/main/java/kr/spring/member/vo/MemberVO.java
@@ -1,13 +1,109 @@
 package kr.spring.member.vo;
 
+import java.sql.Date;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class MemberVO {
 	private int mem_num;
+	@Pattern(regexp="^[A-Za-z0-9]{4,12}$")
 	private String id;
 	private int auth;
-	@NotEmpty
 	private String name;
+	@Pattern(regexp="^[A-Za-z0-9]{4,12}$")
 	private String passwd;
+	private String photo;
+	private String introduction;
+	private Date reg_date;
+	private Date modify_date;
+	private int follow_num; 
+	private int active_mem;
+	private int passive_mem;
+		
+	//비밀번호 변경시 현재 비빌번호를 저장하는 용도로 사용
+	@Pattern(regexp="^[A-Za-z0-9]{4,12}$")
+	private String now_passwd;
+
+	//비밀번호 일치 여부 체크
+	public boolean isCheckedPassword(String userPasswd) {
+		if(auth > 1 && passwd.equals(userPasswd)) {
+			return true;
+		}
+		return false;
+	}
+				
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getAuth() {
+		return auth;
+	}
+	public void setAuth(int auth) {
+		this.auth = auth;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getModify_date() {
+		return modify_date;
+	}
+	public void setModify_date(Date modify_date) {
+		this.modify_date = modify_date;
+	}
+	public int getFollow_num() {
+		return follow_num;
+	}
+	public void setFollow_num(int follow_num) {
+		this.follow_num = follow_num;
+	}
+	public int getActive_mem() {
+		return active_mem;
+	}
+	public void setActive_mem(int active_mem) {
+		this.active_mem = active_mem;
+	}
+	public int getPassive_mem() {
+		return passive_mem;
+	}
+	public void setPassive_mem(int passive_mem) {
+		this.passive_mem = passive_mem;
+	}
+
 }
