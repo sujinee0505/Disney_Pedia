c1b8504c36a7b8da48bde4eff192cd57184a33bb
SUJIN <cainstear@gmail.com>
2022-03-17 17:45:59 +0900

    파일 명 변경 및 리뷰 테이블 sql문 머지

--- springProject/src/main/webapp/sql/table.sql
+++ springProject/src/main/webapp/sql/table.sql
@@ -104,3 +104,28 @@ create table dlist_like(
   constraint like_dmember_fk2 foreign key (mem_num) references dmember (mem_num)
 );
 create sequence dlist_like_seq;
+
+/* 리뷰 테이블 */
+create table dreview(
+  review_num number not null,
+  content clob not null,
+  reg_date date not null,
+  modify_date date,
+  grade_num number not null,
+  mem_num number not null,
+  constraint dreview_pk primary key (review_num),
+  constraint dreview_fk_1 foreign key (grade_num) references dcontents_grade (grade_num),
+  constraint dreview_fk_2 foreign key (mem_num) references dmember (mem_num)
+);
+create sequence dreview_seq;
+
+/* 리뷰 좋아요 테이블 */
+create table dreview_like(
+  rlike_num number not null,
+  review_num number not null,
+  mem_num number not null,
+  constraint dreview_like_pk primary key (rlike_num),
+  constraint dreview_like_fk_1 foreign key (review_num) references dreview (review_num),
+  constraint dreview_like_fk_2 foreign key (mem_num) references dmember (mem_num)
+);
+create sequence dreview_like_seq;
