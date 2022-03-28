78d4f3e47c4eca5e44add155ff2249299e73546e
LEE JINJOO <LEE JINJOO@DESKTOP-N36HSKB>
2022-03-28 18:14:43 +0900

    xml추가 및 오류 수정

--- springProject/src/main/java/kr/spring/comment/controller/CommentController.java
+++ springProject/src/main/java/kr/spring/comment/controller/CommentController.java
@@ -1,24 +1,8 @@
 package kr.spring.comment.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-import kr.spring.comment.service.CommentService;
-import kr.spring.comment.vo.CommentVO;
 
-@Controller
 public class CommentController {
-	private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
+
 	
-	@Autowired
-	private CommentService commentService;
-	
-	//자바빈(vo)초기화
-	@ModelAttribute
-	public CommentVO initCommand() {
-		return new CommentVO();
-	}
 }
